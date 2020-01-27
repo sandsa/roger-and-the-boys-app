3002a2f0592bc8635cdf00542d30598a4d751d88
sandsa <sandsa810@gmail.com>
2020-01-27 15:21:53 -0600

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,42 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		redirectTo: '/home',
+		pathMatch: 'full'
+	},
+	{
+		path: 'home',
+		redirectTo: '/home',
+		pathMatch: 'full'
+	},
+	{
+		path: 'about',
+		redirectTo: '/about',
+		pathMatch: 'full'
+	},
+	{
+		path: 'photos',
+		redirectTo: '/photos',
+		pathMatch: 'full'
+	},
+	{
+		path: 'shows',
+		redirectTo: '/shows',
+		pathMatch: 'full'
+	},
+	{
+		path: 'contact',
+		redirectTo: '/contact',
+		pathMatch: 'full'
+	},
+	{
+		path: '**',
+		redirectTo: '/home'
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PhotosComponent } from './photos/photos.component';
+import { ContactComponent } from './contact/contact.component';
+import { ShowsComponent } from './shows/shows.component';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'photos', component: PhotosComponent},
+  {path: 'shows', component: ShowsComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PhotosComponent,
+    ShowsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} //todo: disable
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
